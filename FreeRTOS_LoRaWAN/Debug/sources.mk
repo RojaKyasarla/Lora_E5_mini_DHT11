55365b7c6edc5932b9038232a8c3d80a71ad4d86
kyasarlaroja <kyasarlaroja@gmail.com>
2024-02-06 17:57:08 -0800

    LoRa-E5-mini

---
+++ FreeRTOS_LoRaWAN/Debug/sources.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+ELF_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+SIZE_OUTPUT := 
+OBJDUMP_LIST := 
+SU_FILES := 
+EXECUTABLES := 
+OBJS := 
+MAP_FILES := 
+S_DEPS := 
+S_UPPER_DEPS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Core/Src \
+Core/Startup \
+Drivers/STM32WLxx_HAL_Driver \
+LoRaWAN/App \
+LoRaWAN/Target \
+Middlewares/FreeRTOS \
+Middlewares/FreeRTOS/prortable/MemMang \
+Middlewares/LoRaWAN/Crypto \
+Middlewares/LoRaWAN/LmHandler \
+Middlewares/LoRaWAN/LmHandler/Packages \
+Middlewares/littlefs \
+Utilities \
+
